bf215dbd86d011d69d864ae54409ea610051b1dd
Aswinbalajik <githubaswin@gmail.com>
2025-05-30 16:31:30 +0530

    sucessfully login

---
+++ src/github/class1.java
@@ -0,0 +1,18 @@
+package github;
+
+public class class1 {
+
+	private void mtd1() {
+
+		System.out.println("aswin");
+	}
+	
+	public static void main(String[] args) {
+		
+		class1 c = new class1();
+		
+		c.mtd1();
+		
+	}
+	
+}
