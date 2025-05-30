a9e6af9b2e29f837f1664e60aeb82d5b2a9564d3
Aswinbalajik <githubaswin@gmail.com>
2025-05-30 16:45:43 +0530

    done

--- src/github/class1.java
+++ src/github/class1.java
@@ -12,12 +12,19 @@ public class class1 {
 		System.out.println("12345");
 	}
 	
+	
+	private void mtd3() {
+
+		System.out.println("hari");
+	}
+	
 	public static void main(String[] args) {
 		
 		class1 c = new class1();
 		
 		c.mtd1();
 		c.mtd2();
+		
 		
 	}
 	
