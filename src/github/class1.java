4b96cb48cb080215a5706e549a47dc31b61ca5e5
Aswinbalajik <githubaswin@gmail.com>
2025-05-30 16:40:27 +0530

    done

--- src/github/class1.java
+++ src/github/class1.java
@@ -7,11 +7,17 @@ public class class1 {
 		System.out.println("aswin");
 	}
 	
+	private void mtd2() {
+
+		System.out.println("12345");
+	}
+	
 	public static void main(String[] args) {
 		
 		class1 c = new class1();
 		
 		c.mtd1();
+		c.mtd2();
 		
 	}
 	
